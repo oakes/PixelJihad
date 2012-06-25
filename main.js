ff4b20672b4cbbb0b49682d1c330a61d30f7dda8
Zach Oakes <zsoakes@gmail.com>
2012-06-25 04:45:50 +0000

    Initial import.

---
+++ main.js
@@ -0,0 +1,232 @@
+// initialize
+window.onload = function() {
+    // add action to the file input
+    var input = document.getElementById('file');
+    input.addEventListener('change', importImage);
+
+    // add action to the encode button
+    var encodeButton = document.getElementById('encode');
+    encodeButton.addEventListener('click', encode);
+
+    // add action to the decode button
+    var decodeButton = document.getElementById('decode');
+    decodeButton.addEventListener('click', decode);
+
+    // hide labels if the browser supports placeholders
+    if (!document.createElement('input').placeholder) {
+        var labels = document.getElementsByTagName('label');
+        for (var i = 0; i < labels.length; i++) {
+            labels[i].style.display = 'none';
+        }
+    }
+};
+
+// this will store the canvas object
+var canvas = null;
+
+// put image in the canvas and display it
+var importImage = function(e) {
+    var reader = new FileReader();
+
+    reader.onload = function(event) {
+        var img = new Image();
+
+        img.onload = function() {
+            canvas = document.getElementById('canvas');
+            var ctx = canvas.getContext('2d');
+            ctx.canvas.width = img.width;
+            ctx.canvas.height = img.height;
+            ctx.drawImage(img, 0, 0);
+
+            decode();
+        };
+
+        img.src = event.target.result;
+        document.getElementById('preview').style.display = 'block';
+        document.getElementById('preview').src = event.target.result;
+    };
+
+    reader.readAsDataURL(e.target.files[0]);
+};
+
+// decode the image and display the contents if there is anything
+var decode = function() {
+    var password = document.getElementById('password2').value;
+    var passwordFail = 'Password is incorrect or there is nothing here.';
+
+    // decode the message with the supplied password
+    var ctx = canvas.getContext('2d');
+    var imgData = ctx.getImageData(0, 0, ctx.canvas.width, ctx.canvas.height);
+    var message = decodeMessage(imgData.data, sjcl.hash.sha256.hash(password));
+
+    // try to parse the JSON
+    try {
+        message = JSON.parse(message);
+    } catch (e) {
+        message = null;
+    }
+
+    // display the appropriate view
+    if (!message) {
+        document.getElementById('choose').style.display = 'block';
+        document.getElementById('messageDecoded').innerHTML = '';
+        document.getElementById('reveal').style.display = 'none';
+
+        if (password.length > 0) {
+            alert(passwordFail);
+        }
+    } else {
+        document.getElementById('choose').style.display = 'none';
+        document.getElementById('reveal').style.display = 'block';
+
+        if (message.ct) {
+            try {
+                message.text = sjcl.decrypt(password, JSON.stringify(message));
+            } catch (e) {
+                alert(passwordFail);
+            }
+        }
+        document.getElementById('messageDecoded').innerHTML = message.text;
+    }
+
+    document.getElementById('password2').value = '';
+};
+
+// encode the image and save it
+var encode = function() {
+    var message = document.getElementById('message').value;
+    var password = document.getElementById('password').value;
+
+    // encrypt the message with supplied password if necessary
+    if (password.length > 0) {
+        message = sjcl.encrypt(password, message);
+    } else {
+        message = JSON.stringify({'text': message});
+    }
+
+    // encode the encrypted message with the supplied password
+    var canvas = document.getElementById('canvas');
+    var ctx = canvas.getContext('2d');
+    var imgData = ctx.getImageData(0, 0, ctx.canvas.width, ctx.canvas.height);
+    encodeMessage(imgData.data, sjcl.hash.sha256.hash(password), message);
+    ctx.putImageData(imgData, 0, 0);
+
+    // view the new image
+    window.location = canvas.toDataURL();
+};
+
+// returns a 1 or 0 for the bit in 'location'
+var getBit = function(number, location) {
+   return ((number >> location) & 1);
+};
+
+// sets the bit in 'location' to 'bit' (either a 1 or 0)
+var setBit = function(number, location, bit) {
+   return (number & ~(1 << location)) | (bit << location);
+};
+
+// returns an array of 1s and 0s for a 2-byte number
+var getBitsFromNumber = function(number) {
+   var bits = [];
+   for (var i = 0; i < 16; i++) {
+       bits.push(getBit(number, i));
+   }
+   return bits;
+};
+
+// returns the next 2-byte number
+var getNumberFromBits = function(bytes, history, hash) {
+    var number = 0, pos = 0;
+    while (pos < 16) {
+        var loc = getNextLocation(history, hash, bytes.length);
+        var bit = getBit(bytes[loc], 0);
+        number = setBit(number, pos, bit);
+        pos++;
+    }
+    return number;
+};
+
+// returns an array of 1s and 0s for the string 'message'
+var getMessageBits = function(message) {
+    var messageBits = [];
+    for (var i = 0; i < message.length; i++) {
+        var code = message.charCodeAt(i);
+        messageBits = messageBits.concat(getBitsFromNumber(code));
+    }
+    return messageBits;
+};
+
+// gets the next location to store a bit
+var getNextLocation = function(history, hash, total) {
+    var pos = history.length;
+    var loc = Math.abs(hash[pos % hash.length] * (pos + 1)) % total;
+    while (true) {
+        if (loc >= total) {
+            loc = 0;
+        } else if (history.indexOf(loc) >= 0) {
+            loc++;
+        } else if ((loc + 1) % 4 === 0) {
+            loc++;
+        } else {
+            history.push(loc);
+            return loc;
+        }
+    }
+};
+
+// encodes the supplied 'message' into the CanvasPixelArray 'colors'
+var encodeMessage = function(colors, hash, message) {
+    // make an array of bits from the message
+    var messageBits = getBitsFromNumber(message.length);
+    messageBits = messageBits.concat(getMessageBits(message));
+
+    // this will store the color values we've already modified
+    var history = [];
+
+    // encode the bits into the pixels
+    var pos = 0;
+    while (true) {
+        // stop encoding when we're done
+        if (pos == messageBits.length) {
+            break;
+        }
+
+        // set the next color value to the next bit
+        var loc = getNextLocation(history, hash, colors.length);
+        colors[loc] = setBit(colors[loc], 0, messageBits[pos]);
+
+        // set the alpha value in this pixel to 255
+        // we have to do this because browsers do premultiplied alpha
+        // see for example: http://stackoverflow.com/q/4309364
+        while ((loc + 1) % 4 !== 0) {
+            loc++;
+        }
+        colors[loc] = 255;
+
+        pos++;
+    }
+};
+
+// returns the message encoded in the CanvasPixelArray 'colors'
+var decodeMessage = function(colors, hash) {
+    // this will store the color values we've already read from
+    var history = [];
+
+    // get the message size
+    var messageSize = getNumberFromBits(colors, history, hash);
+
+    // exit early if the message size in bits is more than the available colors
+    if (messageSize === 0 || messageSize > 1000) {
+        return '';
+    }
+
+    // put each character into an array
+    var message = [];
+    for (var i = 0; i < messageSize; i++) {
+        var code = getNumberFromBits(colors, history, hash);
+        message.push(String.fromCharCode(code));
+    }
+
+    // the characters should parse into valid JSON
+    return message.join('');
+};
